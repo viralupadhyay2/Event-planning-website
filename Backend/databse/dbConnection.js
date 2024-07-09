e90536e208d566ca65a83b5f6b9b44ffd451d601
viralupadhyay2 <viral.upadhyay2021@vitstudent.ac.in>
2024-07-09 07:19:09 +0530

    first commit

---
+++ Backend/databse/dbConnection.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+export const dbConnection=()=>{
+    mongoose.connect(process.env.MONGO_URI,{dbName:"MERNSTACK_EVENT_MESSAGE"}).then(()=>{
+        console.log("Connected to database!");
+    })
+    .catch((err)=>{
+        console.log("Some error occured whlie connecting to database",err);
+    });
+};
